cd851f46c3734526f41685d68ce8c00c4697d3a7
mohammad1774 <mohammad.ahmed1774@gmail.com>
2024-05-08 13:59:13 +0530

    Update rasp_server.py

--- rasp_server.py
+++ rasp_server.py
@@ -1,14 +1,27 @@
-from flask import Flask, Response
+from flask import Flask, Response,request,jsonify
 from picamera import PiCamera
 import time
 import io
 import requests
+from mysql import connector
+from sensors import *
 
 app = Flask(__name__)
 
 # Initialize PiCamera
 camera = PiCamera()
 camera.resolution = (640, 480)  # Set resolution as needed
+
+def get_db_connection():
+    conn = connector.connect(
+        host='up-us-sjo1-mysql-1.db.run-on-seenode.com',
+        port=11550,
+        user='db-hlx8d21axvv7',
+        password='QPDMAVVtbc39RG0l4R0ytGsO',
+        database='db-hlx8d21axvv7'
+    )
+    return conn
+
 
 # Define a generator function to capture frames from PiCamera
 def generate_frames():
@@ -36,6 +49,30 @@ def generate_frames():
 @app.route('/camera_feed')
 def camera_feed():
     return Response(generate_frames(), mimetype='multipart/x-mixed-replace; boundary=frame')
+
+@app.route('/card_registration',methods=['POST'])
+def detail_confirmation():
+    data = request.json
+    
+    username = data.get('username')
+    password = data.get('password')
+
+    conn = get_db_connection()
+    cursor = conn.cursor()
+
+    rfid_register = rfid()
+    register = rfid_register.register(username)
+    if register:
+        cursor.execute('UPDATE users SET rfid = %s WHERE username = %s and password = %s',(register['id'],username,password))
+        conn.commit()
+        conn.close()
+        return jsonify({'registration':'success','id':register['id']})
+    else:
+        return jsonify({'confirmation':'failure'})
+
+
+    
+    
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=80, debug=True)
